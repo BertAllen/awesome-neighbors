e73bc69845a646640de865263671bcb2b8f6452f
Bert Allen <mrlaughingdragon@aol.com>
2016-04-18 11:37:40 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# awesome-neighbors" 
---
+++ app/app.js
@@ -0,0 +1,13 @@
+app.controller("NeighborController", function () {
+
+    function Neighbor(person, address, phone, talents, needs) {
+        this.person = person;
+        this.address = address;
+        this.phone = phone;
+        this.talents = talents;
+        this.needs = needs;
+    }
+
+
+
+});
