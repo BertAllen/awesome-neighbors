7ca92771e3d89bb7bc160a3e68bc1eb959e50ffe
Bert Allen <mrlaughingdragon@aol.com>
2016-04-20 10:41:18 -0600

    adjusting models and input boxes

--- app/app.js
+++ app/app.js
@@ -1,13 +1,47 @@
 app.controller("NeighborController", function ($scope) {
-
-    function Neighbor(person, address, phone, talents, needs) {
-        this.person = person;
-        this.address = address;
-        this.phone = phone;
-        this.talents = talents;
-        this.needs = needs;
+    $scope.neighbors = [];
+    $scope.Neighbor = function ($scope.person) {
+        this.firstName = $scope.person.firstName;
+        this.lastName = $scope.person.lastName;
+        this.address = $scope.person.address;
+        this.phone = $scope.person.phone;
+        this.talents = [];
+        this.needs = [];
+        if ($scope.person.talent1) {
+            this.talents.push($scope.person.talent1);
+        }
+        if ($scope.person.talent2) {
+            this.talents.push($scope.person.talent2);
+        }
+        if ($scope.person.talent3) {
+            this.talents.push($scope.person.talent3);
+        }
+        if ($scope.person.talent4) {
+            this.talents.push($scope.person.talent4);
+        }
+        if ($scope.person.talent5) {
+            this.talents.push($scope.person.talent5);
+        }
+        if ($scope.person.need1) {
+            this.needs.push($scope.person.need1);
+        }
+        if ($scope.person.need2) {
+            this.needs.push($scope.person.need2);
+        }
+        if ($scope.person.need3) {
+            this.needs.push($scope.person.need3);
+        }
+        if ($scope.person.need4) {
+            this.needs.push($scope.person.need4);
+        }
+        if ($scope.person.need5) {
+            this.needs.push($scope.person.need5);
+        }
     }
-
+    $scope.addNeighbor = function ($scope.person) {
+        var human = new Neighbor($scope.person);
+        $scope.neighbors.push(human);
+}
 
 
 });
